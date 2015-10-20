cadd2d107f79632005a176b910e5fa913b593dda
Angelo Luciani <mistereal@gmail.com>
2015-10-20 17:23:41 +0200

    updated
    updated

--- create_jar.bat
+++ create_jar.bat
@@ -1,12 +1,10 @@
 
 
 REM Create a jar file
-color 2
 echo Started
 cd "C:\Users\angelo.luciani\workspace\testhello\src"
 javac *.java
 echo Main-Class: testhello > manifest.txt
 jar cvfm testhello.jar manifest.txt *.class
 java -jar testhello.jar
-color 2
 echo Completed
